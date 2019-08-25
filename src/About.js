68265d14bde52c7e51ae1567ee2fcf25e4ed8f64
BKJang <jp302119@gmail.com>
2019-08-25 18:35:38 +0900

    BrowserRouter, Link

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Introduce</h1>
+      <p>
+        React Router Example.
+      </p>
+    </div>
+  )
+}
+
+export default About;
