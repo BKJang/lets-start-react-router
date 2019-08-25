da580fb36b51639a4590b6e692ec37f63ecd1916
BKJang <jp302119@gmail.com>
2019-08-25 19:12:40 +0900

    param, query

--- src/About.js
+++ src/About.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import qs from 'qs';
 
-function About() {
+function About({ location }) {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const detail = query.detail === 'true';
   return (
     <div>
       <h1>Introduce</h1>
       <p>
         React Router Example.
       </p>
+      {detail && (
+        <p>
+          This is detail contents.
+        </p>
+      )}
     </div>
   )
 }
---
+++ src/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const profileData = {
+  bkjang: {
+    name: '장봉기',
+    description: 'Frontend Engineer'
+  },
+  jhkim: {
+    name: '김지혜',
+    description: 'Backend Engineer'
+  }
+}
+
+function Profile({ match }) {
+  const { username } = match.params;
+  const profile = profileData[username];
+
+  if (!profile) {
+    return <div>존재하지 않는 사용자</div>;
+  }
+  return (
+    <div>
+      <h3>사용자명 : {profile.name}</h3>
+      <h4>소개: {profile.description}</h4>
+    </div>
+  )
+}
+
+export default Profile;
